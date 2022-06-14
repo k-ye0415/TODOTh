30bae2e788038740dde9fb5d092dff3790a18b96
kyj <k_ye@naver.com>
2022-06-14 22:43:50 +0900

    2022-06-14

---
+++ TODOTh/app/src/main/java/com/ioad/todoth/ListAddActivity.java
@@ -0,0 +1,14 @@
+package com.ioad.todoth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_add);
+    }
+}
